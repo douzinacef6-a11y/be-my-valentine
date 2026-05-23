9ad85ae6a501f5afce39eb3a56e8421aca419da0
douzinacef6-a11y <douzinacef6@gmail.com>
2026-05-23 17:15:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy!! nhebek slema:3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -44,7 +44,7 @@ function generateMessage(noCount) {
   const messages = [
     "No",
     "Are you sure?",
-    "Pookie please",
+    "slema please",
     "Don't do this to me :(",
     "You're breaking my heart",
     "I'm gonna cry...",
